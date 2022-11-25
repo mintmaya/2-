2a1340a2de4adbdcf8b5e8e5ed02457d6a4f54bb
mintmaya <118422299+mintmaya@users.noreply.github.com>
2022-11-25 17:24:50 +0900

    Initial commit

---
+++ 2 задание/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_задание
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("Введите первое число: "); 
+                int n = Convert.ToInt32(Console.ReadLine()); 
+                                                             
+                int result;
+                if (n > 123)
+                    result = (n - 123) * 3; 
+                else
+                    result = Math.Abs(n - 123);
+                Console.WriteLine(result);
+            
+        }
+    }
+}
+    
+
